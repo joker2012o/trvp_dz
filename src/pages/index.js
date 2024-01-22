160f9abbb5dee106cd092a2811d0f082b1825271
joker2012o <48907468+joker2012o@users.noreply.github.com>
2024-01-22 16:55:41 +0300

    Delete src directory

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { Home } from "./Home";
-export { FullMenu } from "./FullMenu";
-export { FullDish } from "./FullDish";
-export { AddMenu } from "./AddMenu";
-export { Registration } from "./Registration";
-export { Login } from "./Login";
-export { AddDish } from './AddDish'
